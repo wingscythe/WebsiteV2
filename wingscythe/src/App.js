ba8d83547a990f11aee7b5c42c0e7425b2c9fe87
Ryan Xu <ryanxu@buffalo.edu>
2023-06-03 18:58:52 -0400

    Home Page (Without Animations)

--- wingscythe/src/App.js
+++ wingscythe/src/App.js
@@ -3,21 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aligner">
+      <title>WingScythe</title>
+      <meta charSet="UTF-8" />
+      <link href="https://fonts.googleapis.com/css2?family=Exo&display=swap" rel="stylesheet" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <div id="wrapper">
+        <h1 className="textCentered centgn">WingScythe</h1>
+        <h1 className="textCentered centgd">- Game Development Group -</h1>
+      </div>
     </div>
   );
 }
